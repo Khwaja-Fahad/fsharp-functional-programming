3f23ebfdfa44411c3c79e78ddcff014607137f3e
Muhammad Fahad <m.fahad2001104@gmail.com>
2026-02-28 20:45:59 +0100

    Add data-types project

---
+++ 01-fundamentals/data-types/Program.fs
@@ -0,0 +1,29 @@
+﻿// =======================
+// F# Data Types Examples
+// =======================
+
+// Integer
+let age:int = 22
+printfn "Age: %d" age
+
+// Float
+let height:float = 5.9
+printfn "Height: %.2f" height
+
+// String
+let name:string = "Muhammad Fahad"
+printfn "Name: %s" name
+
+// Boolean
+let isStudent:bool = true
+printfn "Is Student: %b" isStudent
+
+
+// Type Inference (F# detects automatically)
+let score = 95
+let temperature = 36.6
+let message = "F# is powerful"
+
+printfn "Score: %d" score
+printfn "Temperature: %f" temperature
+printfn "Message: %s" message
---
+++ 01-fundamentals/data-types/README.md
@@ -0,0 +1,12 @@
+# Data Types in F#
+
+This project demonstrates basic F# data types.
+
+## Concepts Covered
+- Integer
+- Float
+- String
+- Boolean
+- Type inference
+
+F# automatically infers types unless explicitly specified.
