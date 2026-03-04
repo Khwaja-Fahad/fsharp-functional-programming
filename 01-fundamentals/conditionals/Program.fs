e408221e32aaeae305395bac49799452c31975d6
Muhammad Fahad <m.fahad2001104@gmail.com>
2026-03-05 00:56:21 +0100

    Add conditionals project

---
+++ 01-fundamentals/conditionals/Program.fs
@@ -0,0 +1,60 @@
+﻿// =======================
+// F# Conditionals
+// =======================
+
+//Basic IF
+
+let age = 20
+
+if age >= 18 then
+    printfn "You are an adult"
+
+
+//IF ELSE
+
+let temperature = 15
+
+if temperature > 25 then
+    printfn "It's hot"
+else
+    printfn "It's not hot"
+
+
+
+//IF as an Expression
+
+let number = 7
+
+let result =
+    if number % 2 = 0 then
+        "Even"
+    else
+        "Odd"
+
+printfn "Number is %s" result
+
+
+//Nested IF
+
+let score = 85
+
+if score >= 90 then
+    printfn "Grade A"
+elif score >= 75 then
+    printfn "Grade B"
+elif score >= 60 then
+    printfn "Grade C"
+else
+    printfn "Fail"
+
+
+
+//MATCH
+
+let number1 = 3
+
+match number1 with
+| 1 -> printfn "One"
+| 2 -> printfn "Two"
+| 3 -> printfn "Three"
+| _ -> printfn "Other"
---
+++ 01-fundamentals/conditionals/README.md
@@ -0,0 +1,11 @@
+# Conditionals in F#
+
+This project demonstrates conditional logic in F#.
+
+Topics covered:
+- if expressions
+- if/else statements
+- nested conditions
+- pattern matching (basic match)
+
+In F#, conditionals are expressions and return values.
