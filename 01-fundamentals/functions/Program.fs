e54e8db50b912bce3af90d159eac189749af412e
Muhammad Fahad <m.fahad2001104@gmail.com>
2026-03-06 00:34:53 +0100

    Add functions project

---
+++ 01-fundamentals/functions/Program.fs
@@ -0,0 +1,39 @@
+﻿// Simple function
+
+let square x =
+    x * x
+
+let result = square 5
+
+printfn "Square of 5 = %d" result
+
+
+//Function With Two Parameters
+
+let add a b =
+    a + b
+
+let sum = add 10 7
+
+printfn "Sum = %d" sum
+
+
+//Function Returning String
+
+let checkEven x =
+    if x % 2 = 0 then
+        "Even"
+    else
+        "Odd"
+
+printfn "%s" (checkEven 6)
+printfn "%s" (checkEven 9)
+
+
+
+//Function With Explicit Type
+
+let multiply (a:int) (b:int) : int =
+    a * b
+
+printfn "%d" (multiply 4 6)
---
+++ 01-fundamentals/functions/README.md
@@ -0,0 +1,9 @@
+# Functions in F#
+
+This project demonstrates basic function usage in F#.
+
+Topics covered:
+- defining functions
+- parameters
+- return values
+- using functions with conditionals
