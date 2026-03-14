3eabcaa54e12bb03187a3883fcd4ab56cb689aa0
Muhammad Fahad <m.fahad2001104@gmail.com>
2026-03-14 23:53:59 +0100

    Add even-odd-analyzer project

---
+++ 01-fundamentals/Simple Projects/even-odd-analyzer/Program.fs
@@ -0,0 +1,39 @@
+﻿// ===============================
+// Even / Odd Analyzer
+// ===============================
+
+// Function to classify number as Even or Odd
+
+let ClassifyNumber n =
+    match n % 2 with
+    | 0 -> "Even"
+    | _ -> "Odd"
+
+
+// Function to analyze number size
+
+let analyzeSize n =
+    if n > 10 then
+        "Greater than 10"
+    elif n = 10 then
+        "Equal to 10"
+    else
+      "Less than 10"
+
+
+// Ask user for input
+
+printf "Enter a Number: "
+let input = System.Console.ReadLine()
+let number = int input
+
+
+// Get results
+let kind = ClassifyNumber number
+let size = analyzeSize number
+
+// Print output
+
+printfn ""
+printfn "%d is %s" number kind
+printfn "%d is %s" number size
---
+++ 01-fundamentals/Simple Projects/even-odd-analyzer/README.md
@@ -0,0 +1,26 @@
+# Even / Odd Analyzer
+
+This is a simple F# console project built as part of my Functional Programming practice.
+
+## Purpose
+
+The program takes a number from the user and analyzes:
+
+- whether it is **Even** or **Odd**
+- whether it is **greater than 10**, **equal to 10**, or **less than 10**
+
+## Concepts Used
+
+- functions
+- pattern matching
+- conditionals
+- user input
+- basic operators
+
+## Example Run
+
+Enter a number: 15
+
+Result:
+15 is Odd
+15 is greater than 10
