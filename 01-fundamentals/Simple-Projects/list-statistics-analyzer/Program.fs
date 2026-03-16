8c9502f2da5b10a440a9be688c8e2c217442ee58
Muhammad Fahad <m.fahad2001104@gmail.com>
2026-03-16 02:15:05 +0100

    Add list-statistics-analyzer project

---
+++ 01-fundamentals/Simple-Projects/list-statistics-analyzer/Program.fs
@@ -0,0 +1,61 @@
+﻿// ==============================
+// List Statistics Analyzer
+// ==============================
+
+printfn "Enter Number separated by space: "
+
+let input = System.Console.ReadLine()
+
+// Convert string input into list of integers
+
+let numbers =
+    input.Split(' ')
+    |> Array.map int
+    |> Array.toList
+
+printfn "NUmbers: %A" numbers
+
+// Count numbers
+let listSize = List.length numbers
+printfn "Size of List: %d" listSize
+
+// Sum of Numbers
+let sum = List.sum numbers
+printfn "Sum of numbers: %d" sum
+
+// Average
+
+let average = List.averageBy float numbers
+printfn "Average of numbers: %.2f" average
+
+// Even Numbers 
+
+let evenNumber =
+    numbers
+    |> List.filter (fun x -> x % 2 = 0)
+
+printfn "Even Numbers in List: %A" evenNumber
+
+
+// Odd Numbers 
+
+let oddNumber =
+    numbers
+    |> List.filter (fun x -> x % 2 <> 0)
+
+printfn "Odd Numbers in List: %A" oddNumber
+
+// Square numbers
+let squares =
+    numbers
+    |> List.map (fun x -> x * x)
+
+printfn "Squares: %A" squares
+
+// Maximum
+let maxValue = List.max numbers
+printfn "Maximum: %d" maxValue
+
+// Minimum
+let minValue = List.min numbers
+printfn "Minimum: %d" minValue
---
+++ 01-fundamentals/Simple-Projects/list-statistics-analyzer/README.md
@@ -0,0 +1,33 @@
+# List Statistics Analyzer
+
+This project analyzes a list of numbers entered by the user.
+
+## Features
+
+- Count numbers
+- Calculate sum
+- Calculate average
+- Find even and odd numbers
+- Generate squares of numbers
+- Find maximum and minimum values
+
+## Concepts Used
+
+- Lists
+- Pipelines
+- Map
+- Filter
+- Functional transformations
+
+## Example
+
+Input:
+3 5 8 10 12
+
+Output:
+Count: 5
+Sum: 38
+Average: 7.60
+Even numbers: [8; 10; 12]
+Odd numbers: [3; 5]
+Squares: [9; 25; 64; 100; 144]
