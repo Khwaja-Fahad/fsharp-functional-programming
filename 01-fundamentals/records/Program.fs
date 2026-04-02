b244f6caba405fce313119c490078f4d2f08fc78
Muhammad Fahad <m.fahad2001104@gmail.com>
2026-04-03 00:06:42 +0100

    Add records project

---
+++ 01-fundamentals/records/Program.fs
@@ -0,0 +1,40 @@
+﻿// =======================
+// Records in F#
+// =======================
+
+type Student = {
+    Name: string
+    Age: int
+    Grade: float
+}
+
+// Create records
+let student1 = { Name = "Ali"; Age = 21; Grade = 85.5 }
+let student2 = { Name = "Sara"; Age = 22; Grade = 91.0 }
+
+// Print
+let printStudent s =
+    printfn "Name: %s" s.Name
+    printfn "Age: %d" s.Age
+    printfn "Grade: %.2f" s.Grade
+    printfn "------------------"
+
+printStudent student1
+printStudent student2
+
+// Update record
+let updatedStudent = { student1 with Grade = 90.0 }
+
+printfn "Updated Student:"
+printStudent updatedStudent
+
+// List of records
+let students = [student1; student2]
+
+// Filter students with grade > 90
+let topStudents =
+    students
+    |> List.filter (fun s -> s.Grade > 90.0)
+
+printfn "Top Students:"
+topStudents |> List.iter printStudent
---
+++ 01-fundamentals/records/README.md
@@ -0,0 +1,17 @@
+# Records in F#
+
+This project demonstrates the use of records in F#.
+
+Records are used to group related data into a structured format.
+
+## Concepts Used
+
+- record types
+- immutable updates
+- list of records
+- filtering records
+
+## Example
+
+Student record:
+{Name = "Ali"; Age = 21; Grade = 85.5}
